4dffc791e7e74023b77741481adc8c91c4bc2a65
mirudhunkumar-it21 <mirudhunkumar.it21@bitsathy.ac.in>
2024-02-19 14:40:54 +0530

    first commit

---
+++ shopping/src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import Shopcategory from './Pages/Shopcategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup'
+
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<Shopcategory category="mens" />} />
+          <Route path='/womens' element={<Shopcategory category="womens" />} />
+          <Route path='/kids' element={<Shopcategory category="kids" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
